12b0d3feef55591c7abc1dd1c283f84757387f72
Moloy Ghosh <moloy.ghosh.cse@gmail.com>
2025-11-30 00:46:38 +0600

    create part is completed

---
+++ note-taking-app/src/App.js
@@ -0,0 +1,54 @@
+import {useState} from "react";
+import './App.css';
+
+function App() {
+   const [note,setNote]=useState('');
+   const [notes,setNotes]=useState([
+      {id:1, title: "Note 1"},
+      {id:2, title: "Note 2"}
+   ]);
+
+   const changeNoteHandler=(e)=>{
+      setNote(e.target.value);
+   }
+
+   const submitHandler=(e)=>{
+      e.preventDefault();
+      if(note.trim()===""){
+         return alert(`Please provide a valid note`);
+      }
+      const newNote={
+         id:Date.now()+"",
+         title:note,
+      }
+      setNotes([newNote,...notes]);
+      setNote("");
+   }
+
+  return (
+    <div className="App">
+      <form onSubmit={submitHandler}>
+         <input
+         type="text"
+         value={note}
+         onChange={changeNoteHandler}
+         />
+         <button type="submit">Add Note</button>
+      </form>
+      <h2>All Notes</h2>
+      <div>
+         {notes.map((singleNote)=>(
+            <>
+            <li key={singleNote.id}>
+               {singleNote.title}
+               <button>Update</button>
+               <button>Delete</button>
+            </li>
+            </>
+            ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
